f1c95096568508759fac1506b5e792238a55645b
Feedjer <155850217+Feedjer@users.noreply.github.com>
2024-08-29 14:20:31 +0200

    Update install.js

--- install.js
+++ install.js
@@ -29,6 +29,7 @@ module.exports = {
         path: "app",                // Edit this to customize the path to start the shell from
         message: [
           "pip install gradio devicetorch",
+          "pip install wheel",
           "pip install -r requirements.txt"
         ]
       }
