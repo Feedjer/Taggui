3bc4efa18bcb4140a7de093c37071164246f07b6
Feedjer <155850217+Feedjer@users.noreply.github.com>
2024-08-29 13:17:34 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# Taggui2
+
+A pinokio script for https://github.com/jhc13/taggui
+
--- install.js
+++ install.js
@@ -1,34 +1,37 @@
 module.exports = {
-  run: [{
-    method: "shell.run",
-    params: {
-      message: [
-        "git clone https://github.com/jhc13/taggui app",
-      ]
-    }
-  }, {
-    method: "script.start",
-    params: {
-      uri: "torch.js",
+  run: [
+    // Edit this step to customize the git repository to use
+    {
+      method: "shell.run",
       params: {
-        path: "app",
-        venv: "env",
-        xformers: true
+        message: [
+          "git clone https://github.com/jhc13/taggui app",
+        ]
       }
-    }
-  }, {
-    method: "shell.run",
-    params: {
-      venv: "env",
-      path: "app",
-      message: [
-        "pip install -r requirements.txt"
-      ]
-    }
-  },{
-    method: "notify",
-    params: {
-      html: "Click the 'start' tab to get started!"
-    }
-  }]
+    },
+    // Delete this step if your project does not use torch
+    {
+      method: "script.start",
+      params: {
+        uri: "torch.js",
+        params: {
+          venv: "env",                // Edit this to customize the venv folder path
+          path: "app",                // Edit this to customize the path to start the shell from
+          // xformers: true   // uncomment this line if your project requires xformers
+        }
+      }
+    },
+    // Edit this step with your custom install commands
+    {
+      method: "shell.run",
+      params: {
+        venv: "env",                // Edit this to customize the venv folder path
+        path: "app",                // Edit this to customize the path to start the shell from
+        message: [
+          "pip install gradio devicetorch",
+          "pip install -r requirements.txt"
+        ]
+      }
+    },
+  ]
 }
